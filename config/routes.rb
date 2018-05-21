d7ac6ad5e636b34d821db3763d0d6e4ab808149e
Aaron Masih <aaron@Ashers-MacBook-Pro.local>
2018-05-21 14:13:12 -0400

    added a whole mess of things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :admins
+  resources :cohorts
+  resources :courses
+  resources :teachers
+  resources :students
   get 'welcome/index'
  
   root 'welcome#index'
---
+++ test/models/create_join_table_student_cohort_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CreateJoinTableStudentCohortTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
